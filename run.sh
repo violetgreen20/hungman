e1ed87b6b9d9824acc18bfe3bae4c1268d3b1260
violetgreen20 <violetgreen2023@gmail.com>
2023-11-06 20:38:53 +0200

    First commit

---
+++ run.sh
@@ -0,0 +1,259 @@
+#!/bin/bash
+
+if ! command -v leo &> /dev/null
+then
+	echo "leo is not installed."
+	exit
+fi
+
+PLAYER_ADDRESS=$LEO_ADDRESS
+
+leo run create_game $PLAYER_ADDRESS "{ ch1: 0u8, ch2: 1u8, ch3: 2u8, ch4: 3u8, ch5: 4u8 }"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 4u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: false.private,
+    ch2: false.private,
+    ch3: false.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 2u8
+
+echo "Should have guessed: {
+	ch1: false,
+	ch2: false,
+	ch3: true,
+	ch4: false,
+	ch5: false,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 4u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: false.private,
+    ch2: false.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 1u8
+
+echo "Should have guessed: {
+	ch1: false,
+	ch2: true,
+	ch3: true,
+	ch4: false,
+	ch5: false,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 4u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: false.private,
+    ch2: true.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 0u8
+
+echo "Should have guessed: {
+	ch1: true,
+	ch2: true,
+	ch3: true,
+	ch4: false,
+	ch5: false,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 4u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: true.private,
+    ch2: true.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 5u8
+
+echo "Should have guessed: {
+	ch1: true,
+	ch2: true,
+	ch3: true,
+	ch4: false,
+	ch5: false,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 3u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: true.private,
+    ch2: true.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 6u8
+
+echo "Should have guessed: {
+	ch1: true,
+	ch2: true,
+	ch3: true,
+	ch4: false,
+	ch5: false,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 2u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: true.private,
+    ch2: true.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 7u8
+
+echo "Should have guessed: {
+	ch1: true,
+	ch2: true,
+	ch3: true,
+	ch4: false,
+	ch5: false,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 1u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: true.private,
+    ch2: true.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: false.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 4u8
+
+echo "Should have guessed: {
+	ch1: true,
+	ch2: true,
+	ch3: true,
+	ch4: false,
+	ch5: true,
+}"
+
+leo run make_guess "{
+  owner: aleo10cz8yt9kwyqqvmwdt0hswdplvsc60eey8msxyh3yj8wqq5k5jq9suc7kw4.private,
+  player: aleo132ngrtqmlysat7d65nfjr3k56x6ww87gyyhavehplncyt9ytf58qatyxgn.private,
+  remaining_trials: 1u8.private,
+  word: {
+    ch1: 0u8.private,
+    ch2: 1u8.private,
+    ch3: 2u8.private,
+    ch4: 3u8.private,
+    ch5: 4u8.private
+  },
+  guessed: {
+    ch1: true.private,
+    ch2: true.private,
+    ch3: true.private,
+    ch4: false.private,
+    ch5: true.private
+  },
+  won: false.private,
+  lost: false.private,
+  _nonce: 7682695600942256019076835057941154665170229300882576143371494791587008103234group.public
+}" 3u8
+
+echo "Should have guessed: {
+	ch1: true,
+	ch2: true,
+	ch3: true,
+	ch4: true,
+	ch5: true,
+}"
